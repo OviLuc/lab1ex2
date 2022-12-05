55f3ac4c289af27db503c7fc6857c8b0e66477d0
Lucaci <Lucaci@DESKTOP-0E3OOL2>
2022-12-05 20:48:11 +0200

    Add project files.

---
+++ lab1ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va calcula media aritmetica a trei numere citite de la tastatura */
+            Console.WriteLine("Acest program va calcula media aritmetica a trei numere.");
+            Console.WriteLine("Introduceti primul numar");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al treilea numar");
+            int z = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Rezultatul este: " + (x + y + z) / 3);
+        }
+    }
+}
